e562e19c9859f54d48307d846ddbb288176a33b2
N <86559276+N55039@users.noreply.github.com>
2022-11-04 11:50:04 +0900

    Revert "Scale-haukka"

--- src/pages/top/container.tsx
+++
@@ -1,6 +0,0 @@
-export const topPages: React.FC =()=>{
-  return(
-    <></>
-    // <topPagesPresenter />
-  );
-}
--- src/pages/top/presenter.tsx
+++
@@ -1,5 +0,0 @@
-export const topPagesPresenter: React.FC =()=>{
-  return(
-    <></>
-  );
-}
